87545c47b929c63150dea4a0094eefefc067e705
Matthew Speights <mattcspeights@gmail.com>
2022-10-01 15:11:53 -0500

    boof

--- flaskr/app.py
+++ flaskr/app.py
@@ -3,10 +3,14 @@ from flask import render_template, Blueprint, Flask
 bp = Blueprint('test', __name__)
 
 @bp.route('/')
-def test():
+def home():
 
     return render_template("home.hl")
 
+@bp.route('/loggedIn')
+def homeLoggedIn():
+
+    return 
 app = Flask(__name__)
 app.register_blueprint(bp)
 app.add_url_rule('/', endpoint='test')
