22ece2c822ebab017a9d6b5823c714c3e258b0d8
Matthew Speights <mattcspeights@gmail.com>
2022-10-01 15:49:06 -0500

    boofi

--- flaskr/dataCapture.py
+++ flaskr/dataCapture.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from hashlib import sha256
 import pickle
+import numpy as np
 
 '''
 Frame Data:
@@ -16,12 +17,18 @@ passengers: other users that are riding with
 
 df = pd.read_pickle("userData.pkl")
 
+def storePassowrd(username, password, df):
+    # hash password
 
-print(df)
+    df.loc[np.where(df["User Name"] == username)][password] = password
 
 def findUser(username):
-    print(df[df["User Name"] == username])
+    print(df[df["User Name"] == username]["Password"][0])
 
 df.loc[len(df.index)] = ['matthew', 'test', '12345', 'test', 'test', 'test', 'test']
+
+findUser("matthew")
+
+storePassowrd("matthew", "cheeseBawls", df)
 
 findUser("matthew")
