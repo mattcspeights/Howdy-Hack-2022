a36a2056dc16abf0109e232e8b69f833fd4d1076
Killian Crittenden <Killiancritt@gmail.com>
2022-10-01 14:35:26 -0500

    still working on dataCapture

--- flaskr/dataCapture.py
+++ flaskr/dataCapture.py
@@ -14,9 +14,9 @@ destination:string
 passengers: other users that are riding with 
 '''
 
-df = pd.read_
+df = pd.read_pickle("user_data.csv")
 #pd.DataFrame(columns=['User Name', 'Email', 'Password', 'Seats', 'Origin', 'Destination', 'Passengers'])
 
 print(df)
 
-df.to_csv('user_data.csv', index=False)
+df.to_pickle('user_data.csv')
