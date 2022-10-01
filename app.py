c97e6ef05cb40d13dcae359554b855a71b6b93e4
Matthew Speights <mattcspeights@gmail.com>
2022-10-01 09:38:10 -0500

    flask tests

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import render_template, Blueprint, Flask
+
+bp = Blueprint('test', __name__)
+
+@bp.route('/')
+def test():
+
+    return render_template("template.hl")
+
+app = Flask(__name__)
+app.register_blueprint(bp)
+app.add_url_rule('/', endpoint='test')
