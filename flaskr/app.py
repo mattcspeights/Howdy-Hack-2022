199c68847764cbf017917151bdd157341aa6e633
Matthew Speights <mattcspeights@gmail.com>
2022-10-01 20:34:13 -0500

    boof

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,5 +1,9 @@
+import email
+from urllib import request
+from xml.dom.minidom import Document
 from flask import render_template, Blueprint, Flask
-
+import dataCapture as dc
+import pandas as pd
 bp = Blueprint('main', __name__)
 
 @bp.route('/')
@@ -13,14 +17,18 @@ def home():
     return render_template("home.hl")
 
 @bp.route('/loggedIn')
-def homeLoggedIn():
+def LoggedIn():
 
     return render_template("loggedIn.hl")
 
 @bp.route('/signUp')
-def homeSignUp():
+def SignUp():
+    if request.method == "POST":
+        df = dc.readUserData("userData.pkl")
+        request.form.get(email)
 
-    return render_template("home.hl")
+
+    return render_template("signUp.hl")
 
 @bp.route('/about')
 def about():
@@ -34,5 +42,3 @@ app.add_url_rule('/home', endpoint='home')
 app.add_url_rule('/about', endpoint = 'about')
 app.add_url_rule('/loggedIn', endpoint='loggedIn')
 app.add_url_rule('/signUp', endpoint='signUp')
-
-print("been")
